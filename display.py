8af4c2e48e68ce632ea8fe8022ea0a6abff4f98f
augustinpj <augustinpj458@gmail.com>
2022-05-19 15:04:29 +0530

    created functions for single contact display

--- display.py
+++ display.py
@@ -7,3 +7,7 @@ def display_all(data):
 		temp_data.append([i + 1, data[i]["name"]["first_name"] + " " + data[i]["name"]["middle_name"] + " " + data[i]["name"]["last_name"], data[i]["phone"]["personal"]])
 
 	print(tabulate(temp_data, headers=["#", "Name", "Phone"]))
+
+# display single contact
+def display_contact(contact):
+	print("Name:", contact["name"]["name_preffix"], contact["name"]["first_name"], contact["name"]["middle_name"], contact["name"]["last_name"], contact["name"]["name_suffix"])
--- main.py
+++ main.py
@@ -12,9 +12,23 @@ while(True):
 	print("CONTACTS BOOK")
 	print("=========================")
 
-	command = input("Enter your command or type help to see list of available commands: ").lower()
+	command = input("Enter your command or type help to see list of available commands: ").lower().split()
 
-	if command == "display contacts":
-		display.display_all(data)
-	elif command == "exit":
+	# command analysis -----------------------------------------------------------------
+	if command[0] == "exit":
 		exit()
+
+	# display commands
+	elif command[0] == "display":
+		if command[1] == "contacts":
+			display.display_all(data)
+		elif command[1] == "contact":
+			index = int(command[2]) - 1
+			if index < len(data):
+				display.display_contact(data[index])
+			else:
+				print("Please enter a valid serial number")
+
+	# command not found
+	else:
+		print("Entered command is not found.")
